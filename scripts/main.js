302afc984c54bb8df200167a6f14ccd453f8fb59
Bruno Genaro <bruno.genaro@gmail.com>
2016-01-24 21:10:37 -0500

    Add "AddToCart" functionality
    - Add renderOrder method in order to display each fish and how many
    fishes are being ordered
    - Update render method in order to calculate the total Order amount

--- scripts/main.js
+++ scripts/main.js
@@ -149,9 +149,46 @@ let Header = React.createClass({
     Order
 */
 let Order = React.createClass({
-  render: function() {
+  renderOrder: function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if (!fish) {
+      return <li key={key}>Sorry, fish no longer available!</li>
+    }
+
     return (
-        <p>Order</p>
+      <li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
+  render: function() {
+    let orderIds = Object.keys(this.props.order);
+    let total = orderIds.reduce((prevTotal, key) => {
+      let fish = this.props.fishes[key];
+      let count = this.props.order[key];
+      let isAvailable = fish && fish.status === 'available';
+
+      if (fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+    return (
+        <div className="order-wrap">
+          <h2 className="order-title">Your Order</h2>
+            <ul className="order">
+              {orderIds.map(this.renderOrder)}
+              <li className="total">
+                <strong>Total:</strong>
+                {h.formatPrice(total)}
+              </li>
+            </ul>
+        </div>
     )
   }
 });
