e8d8a699210db561df71b692c273fe60697c59ab
Bruno Genaro <bruno.genaro@gmail.com>
2016-01-27 01:21:01 -0500

    Add Component validation with PropTypes

--- scripts/main.js
+++ scripts/main.js
@@ -177,6 +177,9 @@ let Header = React.createClass({
             <h3 className="tagline"><span>{this.props.tagline}</span></h3>
         </header>
     )
+  },
+  propTypes: {
+    tagline: React.PropTypes.string.isRequired
   }
 });
 
@@ -238,6 +241,11 @@ let Order = React.createClass({
             </CSSTransitionGroup>
         </div>
     )
+  },
+  propTypes: {
+    fishes:          React.PropTypes.object.isRequired,
+    order:           React.PropTypes.object.isRequired,
+    removeFromOrder: React.PropTypes.func.isRequired
   }
 });
 
@@ -277,6 +285,13 @@ let Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+  },
+  propTypes: {
+    addFish:     React.PropTypes.func.isRequired,
+    loadSamples: React.PropTypes.func.isRequired,
+    fishes:      React.PropTypes.object.isRequired,
+    linkState:   React.PropTypes.func.isRequired,
+    removeFish:  React.PropTypes.func.isRequired
   }
 });
 
